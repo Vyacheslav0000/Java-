71b93a25850eb77196bfd0effc97008f67111b1b
Vyacheslav0000 <55128388+Vyacheslav0000@users.noreply.github.com>
2020-01-26 12:12:32 +0300

    Add files via upload

---
+++ ReentrantLockDemo.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo {
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+
+        lock.tryLock();
+
+        lock.lock();
+
+        lock.unlock();
+    }
+}
